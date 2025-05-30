20b9f318a7defc0230e2aeba040b1500bbd5d4a7
akitorahayashi <atora.has75@gmail.com>
2025-05-30 21:43:24 +0900

    mainの分類器の名前のアップデート

--- main.swift
+++ main.swift
@@ -14,11 +14,11 @@ enum ClassifierType: String {
 
     func makeClassifier() -> any ClassifierProtocol {
         switch self {
-        case .binary: BinaryClassificationClassifier()
-        case .multiClass: MultiClassClassificationClassifier()
-        case .multiLabel: MultiLabelClassificationClassifier()
-        case .ovr: OvRClassificationClassifier()
-        case .ovo: OvOClassificationClassifier()
+        case .binary: BinaryClassifier()
+        case .multiClass: MultiClassClassifier()
+        case .multiLabel: MultiLabelClassifier()
+        case .ovr: OvRClassifier()
+        case .ovo: OvOClassifier()
         }
     }
 }
@@ -42,7 +42,7 @@ enum MLModelType: String {
                 .binary: "v6",
                 .multiClass: "v3",
                 .multiLabel: "v1",
-                .ovr: "v20",
+                .ovr: "v23",
                 .ovo: "v1",
             ],
             author: "akitora",
